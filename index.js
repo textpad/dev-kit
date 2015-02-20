0bc3f9000e8446c6020706275d1178d1acab77ea
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2015-02-20 01:40:58 +0000

    added comments + update
    - added comments to module files
    - added method 'mountOptions' to 'lib/config.js', allows namespaced settings
    - removed 'lib/emitter.js'. will use 'Object.create' in the upcoming file 'lib/app.js'

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
+// required modules
 var app = require("./lib/app");
 var utils = require("./lib/utils");
 
+// app constructor
 function tdk ( cwd, argv ) {
   
+  // the main method is just an alias to `app#define`
   function define ( ) {
     this.define.apply(this, arguments);
   }
   
+  // add the app methods
   utils.merge(define, app);
   
+  // initiate the app, deleting the `app#init` method
+  // afterwords to prevent it from being called again
   define.init(cwd, argv);
   delete define.init;
   
+  // return the app
   return define;
   
 }
 
-tdk.app = app;
+// expose the app object as a prototype
+tdk.prototype = tdk.app = tdk.fn = app;
 
+// the `app#init` just sets-up internal stuff, after that
+// user should set up their stuff, then `app#start` should
+// be called, which should run any required tasks
 tdk.start = function ( cwd, argv ) {
   return tdk(cwd, argv).start();
 };
 
+// export
 module.exports = tdk;
